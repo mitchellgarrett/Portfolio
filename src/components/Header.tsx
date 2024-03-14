f8f40184c88c14953ec2132806c6ad34041c75eb
Mitchell Garrett <45768600+mitchellgarrett@users.noreply.github.com>
2024-03-14 11:18:51 -0600

    Fix header alignment

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,10 +4,14 @@ import { Link } from "react-router-dom";
 function Header() {
     return (
         <div className="Header">
-            <p>Mitchell Garrett</p>
-            <Link to="/">Home</Link>
-            <Link to="/howdy">About</Link>
-            <a href="https://github.com/mitchellgarrett">Github</a>
+            <div className="left">
+                <p>Mitchell Garrett</p>
+            </div>
+            <div className="right">
+                <Link to="/">Home</Link>
+                <Link to="/howdy">About</Link>
+                <a href="https://github.com/mitchellgarrett">Github</a>
+            </div>
             <hr />
         </div>
     );
